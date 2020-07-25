6812677ebd628c3aa4e0cf6df0e50510317573ae
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-07-25 09:30:01 +0430

    hotfix

--- root/server/src/main/java/io/kaseb/server/config/CustomPrometheusConfig.java
+++ root/server/src/main/java/io/kaseb/server/config/CustomPrometheusConfig.java
@@ -3,9 +3,11 @@ package io.kaseb.server.config;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-public class PrometheusConfig {
+@Configuration
+public class CustomPrometheusConfig {
     @Bean
     @Primary
     public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
