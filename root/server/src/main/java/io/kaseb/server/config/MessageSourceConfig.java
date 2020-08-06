086aaa5120c44f3d1689bac83644e0e16e3cc862
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-08-06 13:17:26 +0430

    add changelogs and message source files

---
+++ root/server/src/main/java/io/kaseb/server/config/MessageSourceConfig.java
@@ -0,0 +1,22 @@
+package io.kaseb.server.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+/**
+ * @author Seyyed Mahdiyar Zerehpoush
+ */
+@Configuration
+public class MessageSourceConfig {
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
--- root/server/src/main/java/io/kaseb/server/user/model/dto/BaseUserDto.java
+++ root/server/src/main/java/io/kaseb/server/user/model/dto/BaseUserDto.java
@@ -1,5 +1,6 @@
 package io.kaseb.server.user.model.dto;
 
+import io.kaseb.server.authenticate.model.enums.Role;
 import io.kaseb.server.operator.model.entities.OperatorEntity;
 import io.kaseb.server.user.model.entities.UserEntity;
 import lombok.Data;
@@ -8,14 +9,17 @@ import lombok.Data;
 public class BaseUserDto {
     private String id;
     private String username;
+    private Role role;
 
     public BaseUserDto(UserEntity user) {
         this.id = user.getId();
         this.username = user.getUsername();
+        this.role = Role.ADMIN;
     }
 
     public BaseUserDto(OperatorEntity operator) {
         this.id = operator.getId();
         this.username = operator.getUsername();
+        this.role = Role.OPERATOR;
     }
 }
