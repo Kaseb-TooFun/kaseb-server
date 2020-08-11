79ad3a20dbccffb1b1b1f28efd9500c5254fd8c2
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-08-11 16:23:17 +0430

    change hash algorithm

--- root/server/src/main/java/io/kaseb/server/util/HashUtils.java
+++ root/server/src/main/java/io/kaseb/server/util/HashUtils.java
@@ -6,6 +6,7 @@ import org.springframework.util.Base64Utils;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
 @Slf4j
 public class HashUtils {
     private HashUtils() {
@@ -13,12 +14,12 @@ public class HashUtils {
 
     public static String hash(String value) {
         try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.update(value.getBytes(StandardCharsets.UTF_8));
             byte[] md5Bytes = md.digest();
             return Base64Utils.encodeToString(md5Bytes);
         } catch (NoSuchAlgorithmException e) {
-            logger.error("no such algorithm {} base64", "MD5", e);
+            logger.error("no such algorithm {} base64", "SHA-256", e);
         }
         return "";
     }
