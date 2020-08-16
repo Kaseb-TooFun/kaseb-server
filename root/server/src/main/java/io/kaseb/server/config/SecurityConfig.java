adc3b058bc4e3d03cf2fa3242829cb27b6cbe52b
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-08-16 19:10:42 +0430

    add additional CORS header

--- root/server/src/main/java/io/kaseb/server/config/SecurityConfig.java
+++ root/server/src/main/java/io/kaseb/server/config/SecurityConfig.java
@@ -51,8 +51,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Collections.singletonList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin, Authorization", "Content-Type"));
+        configuration.setExposedHeaders(Collections.singletonList("Access-Control-Allow-Origin, Authorization"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
