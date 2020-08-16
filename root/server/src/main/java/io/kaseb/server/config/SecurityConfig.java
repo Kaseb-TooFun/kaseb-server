18149e94fd9921aa937b0c95af0423c89b30453e
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-08-16 19:16:40 +0430

    disable CORS

--- root/server/src/main/java/io/kaseb/server/config/SecurityConfig.java
+++ root/server/src/main/java/io/kaseb/server/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
+        http.cors().disable()
                 .csrf().disable().authorizeRequests()
                 .antMatchers("/monitoring/**").authenticated()
                 .and().httpBasic();
